e66e25aae1b938a43eb6db85efa40498990edab6
Chimipupu <chiminyanyan@gmail.com>
2026-02-20 22:47:43 +0900

    割り込み/時刻/アラーム以外の初期化を実装

--- drv_rtc_rx8900.c
+++ drv_rtc_rx8900.c
@@ -11,19 +11,100 @@
 
 #include "drv_rtc_rx8900.h"
 
-void drv_rtc_rx8900_init(void)
+// -----------------------------------------------------------
+static rtc_rx8900_config_t s_config;
+
+static void rx8900_start(void);
+static void rx8900_stop(void);
+static void rx8900_write_reg(uint8_t reg_addr, uint8_t data);
+static uint8_t rx8900_read_reg(uint8_t reg_addr);
+// -----------------------------------------------------------
+// [Static関数]
+
+static void rx8900_start(void)
 {
-    // TODO:
+    // 拡張レジスタ @Addr=0x0DのBit4のTEをセット
+    s_config.p_func_i2c_write(RTC_RX8900_REG_EXTENSION);
+    uint8_t reg = s_config.p_func_i2c_read();
+    reg |= 0x10; // Bit4をセット
+    s_config.p_func_i2c_write(RTC_RX8900_REG_EXTENSION);
+    s_config.p_func_i2c_write(reg);
 }
 
-void drv_rtc_rx8900_write_reg(uint8_t reg_addr, uint8_t data)
+static void rx8900_stop(void)
 {
-    // TODO:
+    // 拡張レジスタ @Addr=0x0DのBit4のTEをクリア
+    s_config.p_func_i2c_write(RTC_RX8900_REG_EXTENSION);
+    uint8_t reg = s_config.p_func_i2c_read();
+    reg &= ~0x10; // Bit4をクリア
+    s_config.p_func_i2c_write(RTC_RX8900_REG_EXTENSION);
+    s_config.p_func_i2c_write(reg);
 }
 
-uint8_t drv_rtc_rx8900_read_reg(uint8_t reg_addr)
+static void rx8900_write_reg(uint8_t reg_addr, uint8_t data)
 {
-    // TODO:
+    // アドレス: 0x00~0x0F
+    if(((reg_addr >= 0x00) && (reg_addr <= 0x0F))) {
+        if(reg_addr == RTC_RX8900_REG_CTRL) {
+            // Bit7のTESTビットは常に0
+            s_config.p_func_i2c_write(data & 0x7F);
+        } else {
+            s_config.p_func_i2c_write(data);
+        }
+    }
+}
+
+static  uint8_t rx8900_read_reg(uint8_t reg_addr)
+{
+    uint8_t ret = 0x00;
+
+    // アドレス: 0x00~0x0F
+    if(((reg_addr >= 0x00) && (reg_addr <= 0x0F))) {
+        s_config.p_func_i2c_write(reg_addr);
+        ret = s_config.p_func_i2c_read();
+    }
+
+    return ret;
+}
+
+// -----------------------------------------------------------
+// [API]
+
+/**
+ * @brief RTC RX8900初期化関数
+ * @param p_config RX8900初期化構造体ポインタ
+ * @return true 初期化成功
+ * @return false 初期化失敗
+ */
+bool drv_rtc_rx8900_init(rtc_rx8900_config_t *p_config)
+{
+    bool ret = true;
+
+    // 引数のチェック
+    if (p_config == NULL || p_config->p_func_i2c_init == NULL || p_config->p_func_i2c_read == NULL || p_config->p_func_i2c_write == NULL) {
+        ret = false;
+        return ret;
+    }
+
+    memcpy(&s_config, p_config, sizeof(rtc_rx8900_config_t));
+    s_config.p_func_i2c_init(RTC_RX8900_I2C_ADDR);
+
+    // 拡張レジスタ @Addr=0x0D
+    rx8900_write_reg(RTC_RX8900_REG_EXTENSION, 0x00);
+
+    // フラグレジスタ @Addr=0x0E
+    rx8900_write_reg(RTC_RX8900_REG_FLAG, 0x00);
+
+    // コントロールレジスタ @Addr=0x0F
+    // NOTE: Bit0のRESETを1にしてカウントをクリア
+    rx8900_write_reg(RTC_RX8900_REG_CTRL, 0x01);
+
+    // TODO: 割り込み/時刻/アラームの設定
+
+    // RTCスタート(カウント開始)
+    rx8900_start();
+
+    return ret;
 }
 
 void drv_rtc_rx8900_set_time(rtc_rx8900_time_t *p_time)
--- drv_rtc_rx8900.h
+++ drv_rtc_rx8900.h
@@ -16,7 +16,7 @@
 #include <stdbool.h>
 #include <string.h>
 
-#define RTC_RX8900_I2C_ADDR 0x32 // RX8900のI2Cアドレス
+#define RTC_RX8900_I2C_ADDR             0x32 // RX8900のI2Cアドレス
 
 #define RTC_RX8900_REG_SEC              0x00 // 秒
 #define RTC_RX8900_REG_MIN              0x01 // 分
@@ -35,6 +35,17 @@
 #define RTC_RX8900_REG_FLAG             0x0E // フラグレジスタ
 #define RTC_RX8900_REG_CTRL             0x0F // コントロールレジスタ
 
+typedef void (*p_drc_i2c_init)(uint8_t slave_addr);
+typedef void(*p_func_drv_i2c_write)(uint8_t data);
+typedef uint8_t(*p_func_drv_i2c_read)(void);
+
+typedef struct {
+    bool is_interrupt;                     // 割り込み有効フラグ
+    p_drc_i2c_init p_func_i2c_init;        // 呼び元のI2C初期化関数ポインタ
+    p_func_drv_i2c_write p_func_i2c_write; // 呼び元のI2C書き込み関数ポインタ
+    p_func_drv_i2c_read p_func_i2c_read;   // 呼び元のI2C読み取り関数ポインタ
+} rtc_rx8900_config_t;
+
 typedef struct {
     uint8_t sec;      // 秒 (0-59)
     uint8_t min;      // 分 (0-59)
@@ -45,9 +56,7 @@ typedef struct {
     uint16_t year;    // 年 (2000-2099)
 } rtc_rx8900_time_t;
 
-void drv_rtc_rx8900_init(void);
-void drv_rtc_rx8900_write_reg(uint8_t reg_addr, uint8_t data);
-uint8_t drv_rtc_rx8900_read_reg(uint8_t reg_addr);
+bool drv_rtc_rx8900_init(rtc_rx8900_config_t *p_config);
 void drv_rtc_rx8900_set_time(rtc_rx8900_time_t *p_time);
 void drv_rtc_rx8900_get_time(rtc_rx8900_time_t *p_time);
 void drv_rtc_rx8900_set_alarm(rtc_rx8900_time_t *p_time);
